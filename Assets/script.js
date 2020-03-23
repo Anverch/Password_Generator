317a6149795c0ee37c90442a634dd37583b223a7
Aleksandr Saiyan <anverch@gmail.com>
2020-03-22 19:07:52 -0500

    given info

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+function generatePassword() {
+    return hello
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,1 @@
+# 03 JavaScript: Password Generator
