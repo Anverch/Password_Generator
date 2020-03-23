63490e0bb103df8c55892cd95438d93616d4ad0c
Aleksandr Saiyan <anverch@gmail.com>
2020-03-22 22:55:02 -0500

    password conditions update

--- Assets/script.js
+++ Assets/script.js
@@ -9,9 +9,38 @@ function writePassword() {
     passwordText.value = password;
 
 }
-
+// condtions for the password //
 function generatePassword() {
-    return hello
+    var length = parseInt(prompt("How long should your password be?"));
+    var lowerCase = confirm("Include lower case?");
+    var upperCase = confirm("Include upper case?");
+    var numeric = confirm("Include numbers?");
+    var symbols = confirm("Include symbols?");
+
+    var errors = [];
+    if (length < 8 || length > 50 || !length) {
+        errors.push("Length not enough");
+    }
+    if (!lowerCase) {
+        errors.push("must have lower case");
+
+    }
+    if (!upperCase) {
+        errors.push("must have upper case");
+
+    }
+    if (!numeric) {
+        errors.push("must have numbers");
+
+    }
+    if (!symbols) {
+        errors.push("must have symbols");
+
+    }
+    if (errors.length > 0) {
+        return errors.join(", ");
+    }
+    return "password"
 }
 
 // Add event listener to generate button
