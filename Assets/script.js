014e76f034a8051f3b2ee9e92c2080432375d7dd
Aleksandr Saiyan <anverch@gmail.com>
2020-03-25 22:47:02 -0500

    update length break

--- Assets/script.js
+++ Assets/script.js
@@ -12,15 +12,18 @@ function writePassword() {
 // condtions for the password //
 function generatePassword() {
     var length = parseInt(prompt("How long should your password be?"));
+
+
+    var errors = [];
+    if (length < 8 || length > 50 || !length) {
+        return "Length not enough";
+    }
+
     var lowerCase = confirm("Include lower case?");
     var upperCase = confirm("Include upper case?");
     var numeric = confirm("Include numbers?");
     var symbols = confirm("Include symbols?");
 
-    var errors = [];
-    if (length < 8 || length > 50 || !length) {
-        errors.push("Length not enough");
-    }
     if (!lowerCase) {
         errors.push("must have lower case");
 
@@ -37,10 +40,8 @@ function generatePassword() {
         errors.push("must have symbols");
 
     }
-    if (errors.length > 0) {
-        return errors.join(", ");
-    }
-    return "password"
+
+    return "password";
 }
 
 // Add event listener to generate button
