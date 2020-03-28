7b8b86c33276809852cee4f032b73babeb945a01
Aleksandr Saiyan <anverch@gmail.com>
2020-03-27 23:03:07 -0500

    change loop to while

--- Assets/script.js
+++ Assets/script.js
@@ -16,61 +16,48 @@ function generatePassword() {
     //primary condition
     var errors = [];
     if (length < 8 || length > 50 || !length) {
-        return "Length not enough";
+        return "Length is not enough";
     }
     //secondary conditions 
     var lowerCase = confirm("Include lower case?");
     var upperCase = confirm("Include upper case?");
     var numeric = confirm("Include numbers?");
     var symbols = confirm("Include symbols?");
-    //when conditions are false 
-    if (!lowerCase) {
-        errors.push("must have lower case");
 
-    }
-    if (!upperCase) {
-        errors.push("must have upper case");
-
-    }
-    if (!numeric) {
-        errors.push("must have numbers");
-
-    }
-    if (!symbols) {
-        errors.push("must have symbols");
-
-    }
     // defined variables 
     var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
     var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbersCase = "0123456789";
     var symbolsCase = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
-    var randomPasswordLower = "";
-    var randomPasswordUpper = "";
-    var randomPasswordNum = "";
-    var randomPasswordSymbol = "";
+    var finalPassword = "";
 
-    for (var i = 0; i < length; i++) {
-        //generating random lower case
-        var lowerCaseIndex = Math.floor(Math.random() * lowerCaseChars.length);
-        randomPasswordLower = randomPasswordLower + lowerCaseChars[lowerCaseIndex];
+    while (finalPassword.length < length) {
 
-        //generating random upper case
-        var upperCaseIndex = Math.floor(Math.random() * upperCaseChars.length);
-        randomPasswordUpper = randomPasswordUpper + upperCaseChars[upperCaseIndex];
+        if (lowerCase === true) {
+            var lowerCaseIndex = Math.floor(Math.random() * lowerCaseChars.length);
+            finalPassword = finalPassword + lowerCaseChars[lowerCaseIndex];
+        }
 
-        //generating random number
-        var numCaseIndex = Math.floor(Math.random() * numbersCase.length);
-        randomPasswordNum = randomPasswordNum + numbersCase[numCaseIndex];
-
-        //generating random symbol
-        var symbolCaseIndex = Math.floor(Math.random() * symbolsCase.length);
-        randomPasswordSymbol = randomPasswordSymbol + symbolsCase[symbolCaseIndex];
 
     }
 
-    return randomPasswordLower + randomPasswordUpper + randomPasswordNum + randomPasswordSymbol;
+    // for (var i = 0; i < length; i++) {
+    //     //generating random lower case
+
+    //     //generating random upper case
+
+    //     //generating random number
+    //     var numCaseIndex = Math.floor(Math.random() * numbersCase.length);
+    //     randomPasswordNum = randomPasswordNum + numbersCase[numCaseIndex];
+
+    //     //generating random symbol
+    //     var symbolCaseIndex = Math.floor(Math.random() * symbolsCase.length);
+    //     randomPasswordSymbol = randomPasswordSymbol + symbolsCase[symbolCaseIndex];
+
+    // }
+
+    return finalPassword;
 }
 
 // Add event listener to generate button
